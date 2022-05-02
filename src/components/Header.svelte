bb741c59ca2899fd2c3d763b68dac626fa7ad4d2
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-05-02 01:29:45 +0000

    worked nav logic

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,36 @@
 <script>
-  import Nav from './Nav.svelte';
+  let isNavOpen = false;
+
+  function openNav() {
+    isNavOpen = !isNavOpen;
+    
+  }
+
 </script>
 
-<header class="container py-5 flex justify-between items-center">
+<header class="container py-5 flex justify-between items-center relative">
   <h1 class="text-2xl md:text-4xl">Colin's Lego Tracker</h1>
-  <Nav />
-  <p class="md:hidden">Burger Menu</p>
+  <nav class="bg-yellow-400 absolute flex flex-col md:flex-row gap-4 inset-x-0" class:open={isNavOpen}>
+    <a href="/">Home</a>
+    <a href="/login">Login</a>
+  </nav>
+  <p class="md:hidden" on:click={openNav}>Burger Menu</p>
 </header>
+
+<style>
+  /* header {
+    position: relative;
+    z-index: 100;
+  } */
+
+  nav {
+    position: absolute;
+    top: 100%;
+    transform: scale(1, 0);
+  }
+
+  .open {
+    transform: scale(1, 1);
+  }
+
+</style>
