529583210994bd6d8181b5a6d5a3b897ffe88479
Christopher <chris_bernhard@yahoo.com>
2022-05-02 13:51:32 -0500

    worked on mobile menu

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,28 +1,25 @@
 <script>
   let isNavOpen = false;
 
-  function openNav() {
-    isNavOpen = !isNavOpen;
-    
-  }
+  const openNav = () => isNavOpen = !isNavOpen;
 
 </script>
 
-<div class="header-wrapper absolute w-full bg-yellow-400 relative">
+<div class="header-wrapper w-full bg-yellow-400 relative">
   <header class="container py-5 flex justify-between items-center">
     <h1 class="text-2xl md:text-4xl">Colin's Lego Tracker</h1>
 
-    <div class="nav-wrapper absolute bg-yellow-400 inset-x-0" class:open={isNavOpen}>
-      <nav class="container flex flex-col content-around md:flex-row gap-4">
-        <a href="/">Home</a>
-        <a href="/login" class="pb-5">Login</a>
+    <div class="nav-wrapper absolute bg-yellow-400 inset-x-0 md:static" class:open={isNavOpen}>
+      <nav class="container flex flex-col content-around gap-4 md:flex-row md:items-center">
+        <a href="/" on:click={openNav}>Home</a>
+        <a href="/login" class="pb-5 md:pb-0" on:click={openNav}>Login</a>
       </nav>
     </div>
 
-    <div class="md:hidden w-10 h-8 border border-slate-800 flex flex-col justify-around items-center" on:click={openNav}>
-      <div class="h-0.5 w-5/6 bg-slate-800"></div>
-      <div class="h-0.5 w-5/6 bg-slate-800"></div>
-      <div class="h-0.5 w-5/6 bg-slate-800"></div>
+    <div class="w-10 h-6 flex flex-col justify-around items-center md:hidden" on:click={openNav}>
+      <div class="h-0.5 w-5/6 bg-neutral-800 rounded-md"></div>
+      <div class="h-0.5 w-5/6 bg-neutral-800 rounded-sm"></div>
+      <div class="h-0.5 w-5/6 bg-neutral-800 rounded-sm"></div>
     </div>
 
   </header>
@@ -32,22 +29,18 @@
   .nav-wrapper {
     top: 100%;
     transform: scale(1, 0);
+    transform-origin: top;
+    transition: transform .3s ease-in-out;
+  }
+
+  @media (min-width: 768px) {
+    .nav-wrapper {
+      transform: scale(1,1);
+    }
   }
 
   .open {
     transform: scale(1, 1);
   }
-
-  /* .burger-container {
-    width: 2rem;
-    height: 2rem;
-    border: 1px solid #333;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-  } */
-
-
 
 </style>
